67caa7d68cfde114f6a9a1563b0cede3d1db0b7c
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-06 18:13:42 +0800

    feat: add machines typedef

---
+++ src/machines.ts
@@ -0,0 +1,12 @@
+import { NamedAPIResource } from './utility';
+
+export interface Machine {
+    /** The identifier for this resource. */
+    id: number;
+    /** (Item) The TM or HM item that corresponds to this machine. */
+    item: NamedAPIResource;
+    /** (Move) The move that is taught by this machine. */
+    move: NamedAPIResource;
+    /** (VersionGroup) The version group that this machine applies to. */
+    version_group: NamedAPIResource;
+}
