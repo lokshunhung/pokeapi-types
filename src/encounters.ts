3d91fd5c8c2c975cffdc1fe0711799a0f152ffc0
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-06 17:38:51 +0800

    feat: add encounters typedef

---
+++ src/encounters.ts
@@ -0,0 +1,34 @@
+import { Name, NamedAPIResource } from './utility';
+
+export interface EncounterMethod {
+    /** The identifier for this resource. */
+    id: number;
+    /** The name for this resource. */
+    name: string;
+    /** A good value for sorting. */
+    order: number;
+    /** The name of this resource listed in different languages. */
+    names: Name[];
+}
+
+export interface EncounterCondition {
+    /** The identifier for this resource. */
+    id: number;
+    /** The name for this resource. */
+    name: string;
+    /** The name of this resource listed in different languages. */
+    names: Name[];
+    /** (EncounterConditionValue[]) A list of possible values for this encounter condition. */
+    values: NamedAPIResource[];
+}
+
+export interface EncounterConditionValue {
+    /** The identifier for this resource. */
+    id: number;
+    /** The name for this resource. */
+    name: string;
+    /** (EncounterCondition) The condition this encounter condition value pertains to. */
+    condition: NamedAPIResource;
+    /** The name of this resource listed in different languages. */
+    names: Name[];
+}
