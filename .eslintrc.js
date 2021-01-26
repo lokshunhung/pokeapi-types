87c753b43708910373e84a1de9cfffc42b7155df
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-26 17:15:33 +0800

    build: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+const config = {
+    ignorePatterns: ['**/dist/**', '**/tmp/**'],
+    extends: ['vnus/presets/library'],
+    parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+};
+
+module.exports = config;
